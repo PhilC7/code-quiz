e38d7df45a75a18f7845181428d74032f94362d0
Phil Cooper <philjcooper@outlook.com>
2024-01-13 11:49:59 +0000

    declared global variables

---
+++ assets/js/logic.js
@@ -0,0 +1,13 @@
+/******************************
+*   Declare Global Variables  
+******************************/
+
+var startScreen = document.getElementById("start-screen");
+var questionContainer = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
+var choiceContainer = document.getElementById("choices");
+var endScreen = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
+var initials = document.getElementById("initials");
+var submitBtn = document.getElementById("submit");
+var feedbackContainer = document.getElementById("feedback");
