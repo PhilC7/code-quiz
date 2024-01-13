f72121d5ac8a9014e554227a8c83711ea3468a2e
Phil Cooper <philjcooper@outlook.com>
2024-01-13 13:07:37 +0000

    added timer countdown function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,11 +11,71 @@ var finalScore = document.getElementById("final-score");
 var initials = document.getElementById("initials");
 var submitBtn = document.getElementById("submit");
 var feedbackContainer = document.getElementById("feedback");
+var time = document.getElementById("time");
 
 /******************************
 *   Set Variables  
 ******************************/
 
 var score = 0;
-var timer = 75;
-var countdown; //variable to later set timer
+var timeLeft; // Declare timeLeft 
+var countdown; // Declare countdown
+var questionNumber = 0;
+
+
+/******************************
+*   Event Listeners  
+******************************/
+
+// Start game
+startScreen.addEventListener("click", startGame)
+
+/******************************
+*   Start Game  
+******************************/
+function startGame(event) {
+    resetGame();
+    // Hide start screen
+    startScreen.classList.add("hide");
+    // Show question container
+    questionContainer.classList.remove("hide");
+
+}
+
+
+/******************************
+*   Quiz Reset 
+******************************/
+
+function resetGame() {
+    score = 0;
+    timeLeft = 75;
+    time.textContent = timeLeft;
+    timer();
+}
+
+/******************************
+*   Timer Function
+******************************/
+function timer() {
+    countdown = setInterval(function () {
+        timeLeft--
+        time.textContent = timeLeft;
+
+        if (timeLeft === 0) {
+            //add stop game function
+        }
+    }, 1000) // countdown 1 second at a time
+}
+
+
+
+/******************************
+*   Reference to question access - Delete logs when finished
+******************************/
+// access to questions
+console.log(questions[0].title);
+// access to choices
+console.log(questions[0].choices);
+// access to correct answer
+console.log(questions[0].answer);
